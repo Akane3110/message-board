8e389a3353250b5195f630edafa94398cd314a22
EC2 Default User <ec2-user@ip-172-31-3-170.us-east-2.compute.internal>
2020-08-27 05:24:17 +0000

    CRUD messages

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,54 @@
+class MessagesController < ApplicationController
+    def index
+        @messages = Message.all
+    end
+    
+    def show
+        @message = Message.find(params[:id])
+    end
+    
+    def new
+        @message = Message.new
+    end
+    
+    def create
+        @message = Message.new(message_params)
+        
+        if @message.save
+            flash[:success] = 'メッセージが正常に投稿されました！'
+            redirect_to @message
+        else
+            flash.now[:danger] = 'メッセージの投稿に失敗しました（T_T）'
+            render :new
+        end
+    end
+    
+    def edit
+        @message = Message.find(params[:id])
+    end
+    
+    def update
+        if @message.save
+            flash[:success] = 'メッセージが正常に投稿されました！'
+            redirect_to @message
+        else
+            flash.now[:danger] = 'メッセージの投稿に失敗しました（T_T）'
+            render :edit
+        end        
+    end
+    
+    def destroy
+        @message = Message.find(params[:id])
+        @message.destroy
+        
+        flash[:success]='メッセージは葬り去られました。'
+        redirect_to messages_url
+    end
+    
+    private
+    
+    # Strong Parameter
+    def message_params
+        params.require(:message).permit(:content)
+    end
+end
